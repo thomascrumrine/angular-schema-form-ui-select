cc5beb831945f3d9fd168f55fe18e5ce6a99b6dd
Strets123 <strets123@gmail.com>
2015-03-14 15:55:09 +0000

    adding the features for optional descriptions, grouping and tagging along with associated documentation and examples

--- app.js
+++ app.js
@@ -34,6 +34,16 @@ var lightApp = angular.module('lightApp', ['angular-underscore/filters', 'schema
     return $http.get(options.async.url);
   }
 
+  $scope.tagFunction = function(content){
+    var item = {
+      value: content,
+      label: content,
+      description : '',
+      group: ''
+    }
+    return item;
+  };
+
   $scope.schema = {
     type: 'object',
     title: 'Select',
@@ -127,6 +137,105 @@ var lightApp = angular.module('lightApp', ['angular-underscore/filters', 'schema
         minItems: 1,
         maxItems: 2
       },
+      descriptions: {
+        title: 'Multi Select with descriptions and without Search on the description field.',
+        type: 'array',
+        format: 'uiselect',
+        description: 'Multi single items arre allowed',
+        minItems: 1,
+        maxItems: 4,
+        items: [
+          { value: 'label1', label: 'label1', description: 'a long description to provide context that is not useful for search'},
+          { value: 'label2', label: 'label2', description: 'a different long description to provide context that is not useful for search'},
+          { value: 'label3', label: 'label3', description: 'a further long description to provide context that is not useful for search'},
+          { value: 'label4', label: 'label4', description: 'another long description to provide context that is not useful for search'},
+          { value: 'label5', label: 'label5', description: 'yet another long description to provide context that is not useful for search'}
+        ]
+      },
+      descriptions_search: {
+        title: 'Multi Select with search on descriptions',
+        type: 'array',
+        format: 'uiselect',
+        description: 'Multi single items arre allowed, search on description',
+        minItems: 1,
+        items: [
+          { group:'North America', value: 'us', label: 'Canada', description: ''},
+          { group:'North America', value: 'cn', label: 'USA' , description: 'US, USA, United States of America'},
+          { group:'Europe', value: 'gb', label: 'UK' , description: 'United Kingdom, Great Britain, GB'},
+          { group:'Europe', value: 'nl', label: 'Holland' , description: 'Netherlands, NL, Nederland, Pays-Bas'}
+        ]
+      },
+      tagging: {
+        title: 'Tagging',
+        type: 'array',
+        format: 'uiselect',
+        description: 'Hit enter or comma to create a new iootem in the dropdown',
+        minItems: 1,
+        maxItems: 2,
+        items: [
+          { value: 'one', label: 'label1'},
+          { value: 'two', label: 'label2'},
+          { value: 'three', label: 'label3'},
+          { value: 'four', label: 'label4'},
+          { value: 'five', label: 'label5'}
+        ]
+      },
+      grouping: {
+        title: 'Multi Select with grouping',
+        type: 'array',
+        format: 'uiselect',
+        description: 'Can be empty',
+        minItems: 0,
+        items: [
+          { group:'North America', value: 'us', label: 'Canada'},
+          { group:'North America', value: 'cn', label: 'USA' },
+          { group:'Europe', value: 'gb', label: 'UK' },
+          { group:'Europe', value: 'nl', label: 'Holland'}
+        ]
+      },
+      grouping_and_tagging: {
+        title: 'Grouping with Tagging',
+        type: 'array',
+        format: 'uiselect',
+        description: 'Hit enter or comma to create a new item in the dropdown',
+        minItems: 1,
+        maxItems: 2,
+        items: [
+          { group:'North America', value: 'us', label: 'Canada'},
+          { group:'North America', value: 'cn', label: 'USA' },
+          { group:'Europe', value: 'gb', label: 'UK' },
+          { group:'Europe', value: 'nl', label: 'Holland'}
+        ]
+      },
+      single_grouping_and_tagging: {
+        title: 'Single Select with Grouping with Tagging',
+        type: 'string',
+        format: 'uiselect',
+        description: 'Hit enter or comma to create a new item in the dropdown',
+        minItems: 1,
+        maxItems: 2,
+        items: [
+          { group:'North America', value: 'us', label: 'Canada'},
+          { group:'North America', value: 'cn', label: 'USA' },
+          { group:'Europe', value: 'gb', label: 'UK' },
+          { group:'Europe', value: 'nl', label: 'Holland'}
+        ]
+      },
+      single_tagging: {
+        title: 'Tagging',
+        type: 'string',
+        format: 'uiselect',
+        description: 'Hit enter or comma to create a new iootem in the dropdown',
+        minItems: 1,
+        maxItems: 2,
+        items: [
+          { value: 'one', label: 'label1'},
+          { value: 'two', label: 'label2'},
+          { value: 'three', label: 'label3'},
+          { value: 'four', label: 'label4'},
+          { value: 'five', label: 'label5'}
+        ]
+      },
     },
     required: ['staticselect', 'numberselect', 'dyanmicselect', 'staticmultiselect', 'dynamicmultiselect', 'asyncselect', 'multiselect_get', 'multiselect_post']
   };
@@ -144,6 +253,23 @@ var lightApp = angular.module('lightApp', ['angular-underscore/filters', 'schema
        options: {
          refreshDelay: 100,
          callback: $scope.refreshSelect
+       }
+     },
+     {
+       key: 'single_tagging',
+       options: {
+          tagging: $scope.tagFunction ,
+          taggingLabel: '(adding new)',
+          taggingTokens: 'ENTER|,'
+       }
+     },
+      {
+       key: 'single_grouping_and_tagging',
+       options: {
+        groupBy : 'group',
+          tagging: $scope.tagFunction ,
+          taggingLabel: '(adding new)',
+          taggingTokens: 'ENTER|,'
        }
      },
      {
@@ -181,6 +307,36 @@ var lightApp = angular.module('lightApp', ['angular-underscore/filters', 'schema
                url : "http://example:8080/api/rs",
                parameter: { category: "demo",  name: "getDropdown", readOnly: true}
            }
+       }
+     },
+     'descriptions',
+     {
+       key: 'descriptions_search',
+       options:{
+          searchDescriptions : true
+       }
+     },
+     {
+       key: 'tagging',
+       options: {
+          tagging: $scope.tagFunction ,
+          taggingLabel: '(adding new)',
+          taggingTokens: 'ENTER|,'
+       }
+     },
+     {
+       key: 'grouping',
+       options: {
+          groupBy : 'group'
+       }
+     },
+      {
+       key: 'grouping_and_tagging',
+       options: {
+        groupBy : 'group',
+          tagging: $scope.tagFunction ,
+          taggingLabel: '(adding new)',
+          taggingTokens: 'ENTER|,'
        }
      }
   ];
