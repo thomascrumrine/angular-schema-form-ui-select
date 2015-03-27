6d8c765d399389c11519e9ad6f9b4b2e6d30cfae
Steve Hu <stevehu@gmail.com>
2015-03-26 23:00:30 -0400

    work in progress

---
+++ scripts/controllers/menu.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('lightApp').controller('menuCtrl', ['$scope', '$http', 'authService', function($scope, $http, authService) {
+    console.log("now we are in menuCtrl");
+    $scope.menuSettings = {isCollapsed : true};
+    $scope.tree = [];
+
+    var getMenuPost = {
+        category : 'menu',
+        name : 'getMenu',
+        readOnly: true,
+        data : {
+            host : 'example'
+        }
+    };
+
+    $http.post('http://www.networknt.com/api/rs', getMenuPost)
+        .success(function(result, status, headers, config) {
+            $scope.tree = result.out_Own;
+
+        });
+
+    $scope.toggleCollapsed = function () {
+        $scope.menuSettings.isCollapsed =  !$scope.menuSettings.isCollapsed;
+    };
+
+    $scope.hasAccess = function(item) {
+        //console.log('item = ', item);
+        //console.log('currentUser.roles', authService.authentication.currentUser.roles);
+        //console.log('itme.roles', item.roles);
+        for (var i = 0; i < authService.authentication.currentUser.roles.length; i++) {
+            for (var j = 0; j < item.roles.length; j++) {
+                if (authService.authentication.currentUser.roles[i] == item.roles[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+
+    $scope.logOut = function () {
+        authService.logOut();
+
+    };
+
+}]);
